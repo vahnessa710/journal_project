53a2f64a3042121cefafea82d1321102e5661cb4
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-02-14 03:26:58 +0800

    generate User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_05_181832) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_13_191507) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -27,6 +27,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_05_181832) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "tasks", "categories"
