29912a02296f4fbc6bdee1851d50a052fa3cdf9f
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-02-17 19:58:05 +0800

    require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:alert] = "Please log in to continue."
+      redirect_to login_path  # or wherever you want to redirect unauthenticated users
+    end
   end
 end
