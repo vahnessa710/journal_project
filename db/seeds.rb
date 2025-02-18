c3095895ab609d4bf6aca89b072822c1ae7cf9a8
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-02-18 20:40:09 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.find_or_create_by!(email: "test@example.com", password: "password", password_confirmation: "password")
