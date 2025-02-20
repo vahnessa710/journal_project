2c157b58d842dfb6d0a425ca119886b05a9a92c2
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-02-20 20:43:21 +0800

    fixed save bug, updated style, sign up validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   has_many :tasks, through: :categories, dependent: :destroy
 
   validates :email, presence: { message: "Email can't be blank." }, uniqueness: { message: "Email is already taken." }
-  
-  validates :password, presence: { message: "Password can't be blank." }, length: { minimum: 6, message: "Password must be at least 6 characters long." }
-
+  validates :password, length: { minimum: 6, message: "Password must be at least 6 characters long." }
   validates :password_confirmation, presence: { message: "Please confirm your password." }
+
 end
